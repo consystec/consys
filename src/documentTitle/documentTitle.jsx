ae129fbebbd2b7ee11cd7bb9fa8a340773c68a44
joseAlexandre29 <joseantonio_alexandre@hotmail.com>
2020-11-12 16:24:57 -0300

    Adiciona nova formatação no documentTitle

--- src/documentTitle/documentTitle.jsx
+++ src/documentTitle/documentTitle.jsx
@@ -1,11 +1,26 @@
 import Config from 'consys/Config';
+import PropTypes from 'prop-types';
 
 let documentTitle = {
-  set(str) {
-    const title = str.concat(' - ', Config.get('projectName'))
+  set(str, after) {
+    let title = Config.get('projectName');
+
+    if (after) {
+      title = title.concat(' - ', str);
+    } else {
+      title = str.concat(' - ', title)
+    }
+
     var link = document.querySelector("title") || document.createElement('title');
+
     link.innerHtml = title;
     document.title = title;
   }
 };
+
+documentTitle.propTypes = {
+  str: PropTypes.string,
+  after: PropTypes.any
+};
+
 export default documentTitle;
