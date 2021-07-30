a6cbaff465ad2a81d79f46937f43d5ef81850428
joseAlexandre29 <joseantonio_alexandre@hotmail.com>
2021-07-30 13:25:09 -0300

    Publishing to dist (publish.sh)

--- Datepicker.jsx
+++ Datepicker.jsx
@@ -1,30 +1,24 @@
-import React, { Component } from 'react';
-import { DatePicker }  from 'antd'; 
+import React from 'react';
+import { DatePicker } from 'antd';
 
-class _DatePicker extends Component {
-  render() {
-    let {
-      style,
-      format,
-      ...rest
-    } = this.props;
-    if (!style)
-    {
-      style = {};
-    }
-    if (!style.width)
-    {
-      style.width = '100%';
-    }
-    if (!format)
-    {
-      format = "DD/MM/YYYY";
-    }
-    return (
-      <DatePicker {...rest}
-        style={style} 
-        format={format} />
-    );
+function _DatePicker({ style, format, ...rest }) {
+  if (!style) {
+    style = {};
   }
+
+  if (!style.width) {
+    style.width = '100%';
+  }
+
+  if (!format) {
+    format = "DD/MM/YYYY";
+  }
+
+  return (
+    <DatePicker {...rest}
+      style={style}
+      format={format} />
+  );
 }
+
 export default _DatePicker;
