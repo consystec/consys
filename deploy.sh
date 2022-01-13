72310a21fa2a7126f7f43623cc605f3c23f0d456
joseAlexandre29 <joseantonio_alexandre@hotmail.com>
2022-01-13 15:05:48 -0300

    Publishing to dist (publish.sh)

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [[ $(git status -s) ]]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
+# if [[ $(git status -s) ]]
+# then
+#     echo "The working directory is dirty. Please commit any pending changes."
+#     exit 1;
+# fi
 git fetch
 
 echo "Deleting old publication"
@@ -14,7 +14,7 @@ git worktree prune
 rm -rf .git/worktrees/dist
 
 echo "Checking out dist branch into publish"
-git worktree add -B dist publish origin/publish
+git worktree add -B dist new origin/new
 
 echo "Removing existing files"
 rm -rf dist/*
