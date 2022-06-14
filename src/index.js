c8c31ef8c39764112b9b75d61239aa38c5dfd956
Augusto Tomasi <augusto@consystec.com.br>
2022-06-14 17:00:28 -0300

    altera index.js

--- src/index.js
+++ src/index.js
@@ -1,191 +1,59 @@
-import addressBar from './addressBar';
-
-export { addressBar };
-
-import Attach from './Attach';
-
-export { Attach };
-
-import auth from './auth';
-
-export { auth };
-
-import Capitalize from './Capitalize';
-
-export { Capitalize };
-
-import CepInput from './CepInput';
-
-export { CepInput };
-
-import CepTypeahead from './CepTypeahead';
-
-export { CepTypeahead };
-
-import CnpjInput from './CnpjInput';
-
-export { CnpjInput };
-
-import Config from './Config';
-
-export { Config };
-
-import ConsultButton from './ConsultButton';
-
-export { ConsultButton };
-
-import ConsysButton from './ConsysButton';
-
-export { ConsysButton };
-
-import CpfCnpjInput from './CpfCnpjInput';
-
-export { CpfCnpjInput };
-
-import CpfInput from './CpfInput';
-
-export { CpfInput };
-
-import Currency from './Currency';
-
-export { Currency };
-
-import DatePicker from './DatePicker';
-
-export { DatePicker };
-
-import DatepickerRange from './DatepickerRange';
-
-export { DatepickerRange };
-
-import DeleteButton from './DeleteButton';
-
-export { DeleteButton };
-
-import DetailInput from './DetailInput';
-
-export { DetailInput };
-
-import DetailLabel from './DetailLabel';
-
-export { DetailLabel };
-
-import documentTitle from './documentTitle';
-
-export { documentTitle };
-
-import EditableTable from './EditableTable';
-
-export { EditableTable };
-
-import EditButton from './EditButton';
-
-export { EditButton };
-
-import EstadoInput from './EstadoInput';
-
-export { EstadoInput };
-
-import { KEYS_ESTADOS } from './EstadoKeys';
-
-export { KEYS_ESTADOS as EstadoKeys };
-
-import FormattedCep from './FormattedCep';
-
-export { FormattedCep };
-
-import FormattedCNPJ from './FormattedCNPJ';
-
-export { FormattedCNPJ };
-
-import FormattedCPF from './FormattedCPF';
-
-export { FormattedCPF };
-
-import FormattedCpfCnpj from './FormattedCpfCnpj';
-
-export { FormattedCpfCnpj };
-
-import FormattedMask from './FormattedMask';
-
-export { FormattedMask };
-
-import FormattedPhone from './FormattedPhone';
-
-export { FormattedPhone };
-
-import http from './http';
-
-export { http };
-
-import InfoTooltip from './InfoTooltip';
-
-export { InfoTooltip };
-
-import InputNumber from './InputNumber';
-
-export { InputNumber };
-
-import InputSearch from './InputSearch';
-
-export { InputSearch };
-
-import Loading from './Loading';
-
-export { Loading };
-
-import Lookup from './Lookup';
-
-export { Lookup };
-
-import MaskInput from './MaskInput';
-
-export { MaskInput };
-
-import NewButton from './NewButton';
-
-export { NewButton };
-
-import NotFound from './NotFound';
-
-export { NotFound };
-
-import NoUser from './NoUser';
-
-export { NoUser };
-
-import PercentageInput from './PercentageInput';
-
-export { PercentageInput };
-
-import PhoneInput from './PhoneInput';
-
-export { PhoneInput };
-
-import PrivateRoute from './PrivateRoute';
-
-export { PrivateRoute };
-
-import RgInput from './RgInput';
-
-export { RgInput };
-
-import Screen from './Screen';
-
-export { Screen };
-
-import Table from './Table';
-
-export { Table };
-
-import Truncate from './Truncate';
-
-export { Truncate };
-
-import Typeahead from './Typeahead';
-
-export { Typeahead };
-
-import ValueMaskInput from './ValueMaskInput';
-
-export { ValueMaskInput };
+export { addressBar } from './addressBar';
+export { ArraySum } from './ArraySum';
+export { AsyncComponent } from './AsyncComponent';
+export { Attach } from './Attach';
+export { auth } from './auth';
+export { build } from './build';
+export { callback } from './callback';
+export { Capitalize } from './Capitalize';
+export { CardItem } from './cardItem';
+export { CardInput } from './cardInput';
+export { CepInput } from './CepInput';
+export { CepTypeahead } from './CepTypeahead';
+export { CheckboxOptions } from './checkbox'
+export { clipboard } from './clipboard';
+export { CnpjInput } from './CnpjInput';
+export { Config } from './Config';
+export { ConsultButton } from './ConsultButton';
+export { ConsysButton } from './ConsysButton';
+export { CpfCnpjInput } from './CpfCnpjInput';
+export { CpfInput } from './CpfInput';
+export { Currency } from './Currency';
+export { DataUtils } from './dataUtils';
+export { DateInput } from './dateInput';
+export { DatePicker } from './DatePicker';
+export { DatepickerRange } from './DatepickerRange';
+export { DeleteButton } from './DeleteButton';
+export { DetailInput } from './DetailInput';
+export { DetailLabel } from './DetailLabel';
+export { documentTitle } from './documentTitle';
+export { EditableTable } from './EditableTable';
+export { EditButton } from './EditButton';
+export { EstadoInput } from './EstadoInput';
+export { KEYS_ESTADOS as EstadoKeys } from './EstadoKeys';
+export { FormattedCep } from './FormattedCep';
+export { FormattedCNPJ } from './FormattedCNPJ';
+export { FormattedCPF } from './FormattedCPF';
+export { FormattedCpfCnpj } from './FormattedCpfCnpj';
+export { FormattedMask } from './FormattedMask';
+export { FormattedPhone } from './FormattedPhone';
+export { http } from './http';
+export { InfoTooltip } from './InfoTooltip';
+export { InputNumber } from './InputNumber';
+export { InputSearch } from './InputSearch';
+export { Loading } from './Loading';
+export { Lookup } from './Lookup';
+export { MaskInput } from './MaskInput';
+export { NewButton } from './NewButton';
+export { NotFound } from './NotFound';
+export { NoUser } from './NoUser';
+export { PercentageInput } from './PercentageInput';
+export { PhoneInput } from './PhoneInput';
+export { PrivateRoute } from './PrivateRoute';
+export { RgInput } from './RgInput';
+export { Screen } from './Screen';
+export { Table } from './Table';
+export { Truncate } from './Truncate';
+export { Typeahead } from './Typeahead';
+export { ValueMaskInput } from './ValueMaskInput';
+export { utilsCss } from './utils.css';
