a096de079acbbfc068ca699c00aaae9da7593685
Augusto Tomasi <augusto@consystec.com.br>
2022-12-30 15:28:39 -0300

    Publishing to dist (publish.sh)

--- DetailInput.jsx
+++ DetailInput.jsx
@@ -22,7 +22,7 @@ function DetailInput({ title, children, extra }) {
 }
 
 DetailInput.propTypes = {
-  title: PropTypes.string,
+  title: PropTypes.node,
   children: PropTypes.node,
   extra: PropTypes.node
 };
